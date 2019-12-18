70a93bd231914e51bab893b3278524dd85dac044
wibu <wibu@itu.dk>
2019-12-19 00:22:31 +0100

    upup

--- Algo/Sorter.java
+++ Algo/Sorter.java
@@ -8,29 +8,29 @@ public class Sorter<T extends Comparable<T>> {
     private Order order;
     private Algorithm algorithm;
 
-    public Sorter(List<T> list, Algorithm algorithm, Order order) {
+    public Sorter(final List<T> list, final Algorithm algorithm, final Order order) {
         arr = (T[]) new Comparable[list.size()];
         arr = list.toArray(arr);
         this.algorithm = algorithm;
         this.order = order;
     }
 
-    public Sorter(List<T> list, Algorithm algorithm) {
+    public Sorter(final List<T> list, final Algorithm algorithm) {
         this(list, algorithm, Order.ASC);
     }
 
-    public Sorter(List<T> list, Order order) {
+    public Sorter(final List<T> list, final Order order) {
         this(list, Algorithm.QUICK_SORT, order);
     }
-    
-    public Sorter(List<T> list) {
+
+    public Sorter(final List<T> list) {
         this(list, Order.ASC);
     }
 
     public List<T> getList() {
         if (list == null) {
             list = new ArrayList<>();
-            for (Comparable obj : arr)
+            for (final Comparable obj : arr)
                 list.add((T) obj);
         }
 
@@ -45,12 +45,17 @@ public class Sorter<T extends Comparable<T>> {
     }
 
     public void sort() {
-        switch(algorithm) {
-            case SELECTION_SORT: selectionSort();
-            case BUBBLE_SORT: bubbleSort();
-            case INSERTION_SORT: insertionSort();
-            case MERGE_SORT: mergeSort();
-            case QUICK_SORT: quickSort();
+        switch (algorithm) {
+        case SELECTION_SORT:
+            selectionSort();
+        case BUBBLE_SORT:
+            bubbleSort();
+        case INSERTION_SORT:
+            insertionSort();
+        case MERGE_SORT:
+            mergeSort(0, arr.length - 1);
+        case QUICK_SORT:
+            quickSort(0, arr.length - 1);
         }
     }
 
@@ -92,50 +97,41 @@ public class Sorter<T extends Comparable<T>> {
         }
     }
 
-    private void mergeSort() {
-        mergeSort(0, arr.length - 1);
-    }
-
-    private void mergeSort(int l, int r) {
+    private void mergeSort(final int l, final int r) {
         if (r <= l)
             return;
 
-        int m = (l + r) / 2;
+        final int m = (l + r) / 2;
         mergeSort(l, m);
         mergeSort(m + 1, r);
         merge(l, m, r);
     }
 
-    private void merge(int l, int m, int r) {
-        Comparable[] left = subArray(l, m);
-        Comparable[] right = subArray(m + 1, r);
+    private void merge(final int l, final int m, final int r) {
+        final Comparable[] left = subArray(l, m);
+        final Comparable[] right = subArray(m + 1, r);
 
         int leftI = 0;
         int rightI = 0;
 
-        for (int i = l; i <= r; i++) {
+        for (int i = l; i <= r; i++)
             if (leftI != left.length && (rightI == right.length || isBefore(left[leftI], right[rightI])))
                 arr[i] = left[leftI++];
             else
                 arr[i] = right[rightI++];
-        }
     }
 
-    private void quickSort() {
-        quickSort(0, arr.length - 1);
-    }
-
-    private void quickSort(int low, int high) {
+    private void quickSort(final int low, final int high) {
         if (low >= high)
             return;
 
-        int pivotIndex = partition(low, high);
+        final int pivotIndex = partition(low, high);
         quickSort(low, pivotIndex - 1);
         quickSort(pivotIndex + 1, high);
     }
 
-    private int partition(int low, int high) {
-        Comparable pivot = arr[high];
+    private int partition(final int low, final int high) {
+        final Comparable pivot = arr[high];
 
         int i = low - 1;
 
@@ -150,39 +146,42 @@ public class Sorter<T extends Comparable<T>> {
         return i + 1;
     }
 
-    private void swap(int i, int j) {
+    private void swap(final int i, final int j) {
         if (i < 0 || j < 0 || i >= arr.length || j >= arr.length)
             throw new IllegalArgumentException("Invalid indeces [" + i + "," + j + "] for list of " + arr.length);
 
-        Comparable temp = arr[i];
+        final Comparable temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    private boolean isBefore(int i, int j) {
+    private Comparable[] subArray(final int from, final int to) {
+        if (from < 0 || to < 0 || from >= arr.length || to >= arr.length)
+            throw new IllegalArgumentException("Invalid indeces [" + from + "," + to + "] for list of " + arr.length);
+
+        final Comparable[] slice = new Comparable[to - from + 1];
+        for (int i = 0; i < slice.length; i++)
+            slice[i] = arr[from + i];
+
+        return slice;
+    }
+
+    private boolean isBefore(final int i, final int j) {
         if (i < 0 || j < 0 || i >= arr.length || j >= arr.length)
             throw new IllegalArgumentException("Invalid indeces [" + i + "," + j + "] for list of " + arr.length);
 
         return isBefore(arr[i], arr[j]);
     }
 
-    private boolean isBefore(Comparable a, Comparable b) {
-        int compared = a.compareTo(b);
+    private boolean isBefore(final Comparable a, final Comparable b) {
+        final int compared = a.compareTo(b);
         switch (order) {
-            case ASC: return compared < 0;
-            case DESC: return compared > 0;
-            default: return false;
+        case ASC:
+            return compared < 0;
+        case DESC:
+            return compared > 0;
+        default:
+            return false;
         }
-    }
-
-    private Comparable[] subArray(int from, int to) {
-        if (from < 0 || to < 0 || from >= arr.length || to >= arr.length)
-            throw new IllegalArgumentException("Invalid indeces [" + from + "," + to + "] for list of " + arr.length);
-
-        Comparable[] slice = new Comparable[to - from + 1];
-        for (int i = 0; i < slice.length; i++)
-            slice[i] = arr[from + i];
-
-        return slice;
     }
 }
